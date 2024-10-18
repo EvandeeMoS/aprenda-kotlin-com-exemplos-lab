ab0df04abad99b748168bf3c1ed7ee60026cce0f
Evandee Silva <47956743+EvandeeMoS@users.noreply.github.com>
2024-10-18 17:14:15 -0300

    Update desafio.kt

--- desafio.kt
+++ desafio.kt
@@ -2,20 +2,137 @@
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+data class Usuario(val nome: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracaoEmMinutos: Int, val nivel: Nivel)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, val conteudos: MutableList<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
-    
+    var dificuldadeMedia: Nivel = calcularDificuldadeMedia()
+
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+    }
+
+    fun matricularVarios(vararg usuarios: Usuario) {
+        for (usuario in usuarios) {
+            inscritos.add(usuario)
+        }
+    }
+
+    fun adicionarConteudo(conteudo: ConteudoEducacional) {
+        conteudos.add(conteudo)
+        dificuldadeMedia = calcularDificuldadeMedia()
+    }
+
+    fun adicionarVariosConteudos(vararg listaConteudos: ConteudoEducacional) {
+        for (conteudo in listaConteudos) {
+            conteudos.add(conteudo)
+        }
+        dificuldadeMedia = calcularDificuldadeMedia()
+    }
+
+    private fun calcularDificuldadeMedia(): Nivel {
+        return conteudos.let {
+            val dificuldadeMap: MutableMap<Nivel, Int> = mutableMapOf(
+                Nivel.BASICO to 0,
+                Nivel.INTERMEDIARIO to 0,
+                Nivel.DIFICIL to 0
+            )
+            for (conteudo in it) {
+                dificuldadeMap[conteudo.nivel] = dificuldadeMap.getValue(conteudo.nivel) + 1
+            }
+            val quantidadeBasico = dificuldadeMap[Nivel.BASICO]!!
+            val quantidadeIntermediario = dificuldadeMap[Nivel.INTERMEDIARIO]!!
+            val quantidadeDificil = dificuldadeMap[Nivel.DIFICIL]!!
+            val divisor = (quantidadeBasico + quantidadeIntermediario + quantidadeDificil).let { sum ->
+                when (sum) {
+                    0 -> 1
+                    else -> sum
+                }
+            }
+            val result = (1 * quantidadeBasico + 2 * quantidadeIntermediario + 3 * quantidadeDificil) / divisor
+            when {
+                result <= 1 -> Nivel.BASICO
+                result <= 2 -> Nivel.INTERMEDIARIO
+                else -> Nivel.DIFICIL
+            }
+        }
     }
 }
 
+data class FormacaoDTO(
+    val nome: String,
+    val conteudos: List<ConteudoEducacional>,
+    val dificuldadeMedia: Nivel,
+    val inscritos: List<Usuario>) {
+    constructor(formacao: Formacao) : this(formacao.nome, formacao.conteudos, formacao.dificuldadeMedia, formacao.inscritos)
+}
+
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val aula1 = ConteudoEducacional("Introdução a cálculo aplicado a programação", 30, Nivel.BASICO)
+    val aula2 = ConteudoEducacional("As bases do cálculo", 60, Nivel.INTERMEDIARIO)
+
+    val formacao1 = Formacao("Cálculo aplicado a programação", mutableListOf())
+    formacao1.adicionarVariosConteudos(aula1, aula2)
+
+    // Esperado o output da formação e seus conteúdos adicionados
+    /* Output esperado:
+        Formacao(
+          nome=Cálculo aplicado a programação,
+          conteudos=[
+              ConteudoEducacional(
+                  nome=Introdução a cálculo aplicado a programação,
+                  duracaoEmMinutos=30,
+                  nivel=BASICO),
+              ConteudoEducacional(
+                  nome=As bases do cálculo,
+                 duracaoEmMinutos=60,
+                 nivel=INTERMEDIARIO)
+          ]
+        ) */
+    println(formacao1)
+
+    val pedro = Usuario("Pedro")
+    val maria = Usuario("Maria")
+
+    val aula3 = ConteudoEducacional("Limite a fundo", 60, Nivel.DIFICIL)
+
+    formacao1.adicionarConteudo(aula3)
+    formacao1.matricular(pedro)
+    formacao1.matricular(maria)
+
+    // Esperado o output da formação com o novo conteúdo adicionado seguido do output dos inscritos na formação e com a dificuldade atualizada
+    /* Output esperado:
+        Formacao(
+            nome=Cálculo aplicado a programação,
+            conteudos=[
+                ConteudoEducacional(
+                  nome=Introdução a cálculo aplicado a programação,
+                  duracaoEmMinutos=30,
+                  nivel=BASICO),
+              ConteudoEducacional(
+                  nome=As bases do cálculo,
+                 duracaoEmMinutos=60,
+                 nivel=INTERMEDIARIO),
+                ConteudoEducacional(
+                    nome=Limite a fundo,
+                    duracaoEmMinutos=60,
+                    nivel=DIFICIL)
+            ]
+        )
+        [
+            Usuario(nome=Pedro),
+            Usuario(nome=Maria)
+        ]
+        INTERMEDIARIO
+        */
+    println(formacao1)
+    println(formacao1.inscritos)
+    println(formacao1.dificuldadeMedia)
+
+    // Uso de um DTO para tornar o output único
+    val formacaoDto = FormacaoDTO(formacao1)
+    println(formacaoDto)
 }
